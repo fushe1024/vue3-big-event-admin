70300b8b86e70ef21a47a25ae0a3b3babaaaaa22
EvanCookie <2384485535@qq.com>
2025-04-20 20:39:34 +0800

    frist

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+
+  // vue
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 80,
+          trailingComma: 'none',
+          endOfLine: 'auto'
+        }
+      ],
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+    },
+  },
+
+  // 自定义规则
+  {
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 80,
+          trailingComma: 'none',
+          endOfLine: 'auto'
+        }
+      ],
+      'no-undef': 'error',
+      'no-console': 'error'
+    }
+  }
+])
